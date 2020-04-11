06f533bede9aa1eb37f21b4ab2424a89a4f589aa
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-04-11 17:27:38 +0200

    inspector (lib) add streams_detector to kwargs

--- lib/ambix/inspector.rb
+++ lib/ambix/inspector.rb
@@ -17,8 +17,8 @@ class Ambix::Inspector
   }.each { |s, fp| autoload(s, "#{__dir__}/inspector/#{fp}") }
   # @formatter:on
 
-  def initialize
-    @streams_detector = StreamsDetector.new
+  def initialize(streams_detector: nil)
+    @streams_detector = streams_detector || StreamsDetector.new
   end
 
   # @param [String] file
