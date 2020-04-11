43f90688f0ec86aea6be87dfe8325185e5015184
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-04-11 15:10:47 +0200

    track (inspector) major changes and improvements

--- lib/ambix/inspector/track.rb
+++ lib/ambix/inspector/track.rb
@@ -8,7 +8,7 @@
 
 require_relative '../inspector'
 
-# Inspect given file.
+# Describe a track (or stream given ffmpeg vocable).
 class Ambix::Inspector::Track
   # @return [String]
   attr_reader :language
@@ -16,13 +16,39 @@ class Ambix::Inspector::Track
   # @return [String]
   attr_reader :type
 
-  def initialize(**kwargs)
-    self.class.__send__(:defaults).merge(kwargs).each do |k, v|
-      next unless respond_to?("#{k}=", true)
-
-      self.__send__("#{k}=", v)
+  class << self
+    # Defaults used with kwargs.
+    #
+    # @return [Hash{Symbol => Object}]
+    #
+    # @see Track#initialize
+    def defaults(**kwargs)
+      # @formatter:off
+      {
+        index: nil,
+        language: nil,
+        type: nil,
+        default: false,
+        forced: false,
+      }.sort.to_h.tap do |defaults|
+        kwargs.each { |k, v| defaults[k] = v if defaults.key?(k) }
+      end
+      # @formatter:on
     end
   end
+
+  # rubocop:disable Layout/LineLength
+
+  # @option kwargs [String] :index
+  # @option kwargs [String] :language
+  # @option kwargs [String] :type for example ``video``, ``audio`` or ``subtitle``
+  # @option kwargs [Boolean] :default
+  # @option kwargs [Boolean] :forced
+  def initialize(**kwargs)
+    self.class.defaults(**kwargs).each { |k, v| self.__send__("#{k}=", v) }
+  end
+
+  # rubocop:enable Layout/LineLength
 
   class Error < ::StandardError
   end
@@ -68,25 +94,9 @@ class Ambix::Inspector::Track
     end
   end
 
-  class << self
-    protected
-
-    def defaults
-      # @formatter:off
-      {
-        index: nil,
-        language: nil,
-        type: nil,
-        default: nil,
-        forced: nil,
-      }.sort.to_h
-      # @formatter:on
-    end
-  end
-
-  # @return [Hash<Symbol => String|Boolean>]
+  # @return [Hash<Symbol => String|Boolean|Object>]
   def to_h
-    self.class.__send__(:defaults).keys.sort.map do |k|
+    self.class.defaults.keys.sort.map do |k|
       [k, self.__send__(k).freeze]
     end.to_h
   end
