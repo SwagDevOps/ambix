83c0da7ad378006bbb280409f8f3190d58aa8121
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-04-18 19:27:51 +0200

    container (config) container config introduced

---
+++ lib/ambix/config/container.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+# rubocop:disable Layout/LineLength
+
+register(:shell, memoize: true) { Ambix::Shell.new }
+register(:streams_detector, memoize: true) { Ambix::Inspector::StreamsDetector.new }
+
+# rubocop:enable Layout/LineLength
