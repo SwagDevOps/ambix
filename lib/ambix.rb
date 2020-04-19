d3c07cc47413421bce8d36b9cb3039db8796ec22
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-04-19 12:04:35 +0200

    ambix (lib) update autoload + singleton + add container

--- lib/ambix.rb
+++ lib/ambix.rb
@@ -8,11 +8,20 @@
 
 $LOAD_PATH.unshift(__dir__)
 
+autoload(:Singleton, 'singleton')
+autoload(:Pathname, 'pathname')
+
 # Base module.
-module Ambix
+class Ambix
+  # noinspection RubyResolve
+  include Singleton
+
   # @formatter:off
   {
     Bundled: 'bundled',
+    Container: 'container',
+    HasContainer: 'has_container',
+    Injectable: 'injectable',
     Inspector: 'inspector',
     Shell: 'shell',
     VERSION: 'version',
@@ -22,5 +31,18 @@ module Ambix
   include(Bundled).tap do
     require 'bundler/setup' if bundled?
     require 'kamaze/project/core_ext/pp' if development?
+  end
+
+  include(HasContainer)
+
+  class << self
+    # Clear singleton instance.
+    #
+    # @see https://github.com/JuanitoFatas/ruby-2.2.2-standard-library/blob/master/singleton.rb
+    def clear
+      self.tap do
+        @singleton__mutex__.synchronize { @singleton__instance__ = nil }
+      end
+    end
   end
 end
