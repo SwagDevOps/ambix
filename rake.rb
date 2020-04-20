a4ea3f2799b24b53e572205012a6fe74539f0b67
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-04-20 06:59:19 +0200

    add coverage support

--- rake.rb
+++ rake.rb
@@ -4,11 +4,13 @@ require_relative 'lib/ambix'
 
 require 'rake'
 require 'kamaze/project'
+autoload(:YAML, 'yaml')
 
-Kamaze.project do |c|
+Kamaze::Project.instance do |c|
   c.subject = Ambix
   c.name = 'ambix'
-  #noinspection RubyLiteralArrayInspection
+  # @formatter:off
+  # noinspection RubyLiteralArrayInspection
   c.tasks = [
     'cs', 'cs:pre-commit',
     'doc', 'doc:watch',
@@ -19,12 +21,25 @@ Kamaze.project do |c|
     'test',
     'version:edit',
   ].shuffle
+  # @formatter:on
 end.load!
 
 task default: [:gem]
 
+# @type [Kamaze::Project] project
 if project.path('spec').directory?
-  task :spec do |task, args|
+  task :spec do |_task, args|
     Rake::Task[:test].invoke(*args.to_a)
+  end
+end
+
+if Gem::Specification.find_all_by_name('simplecov').any?
+  if YAML.safe_load(ENV['coverage'].to_s) == true
+    autoload(:SimpleCov, 'simplecov')
+
+    SimpleCov.start do
+      add_filter 'rake/'
+      add_filter 'spec/'
+    end
   end
 end
