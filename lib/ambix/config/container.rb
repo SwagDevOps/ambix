ddf5aca483d1f4b676aa00e001952634fa6d5aba
Dimitri Arrigoni <dimitri@arrigoni.me>
2020-04-21 20:46:54 +0200

    container (config) inspector added

--- lib/ambix/config/container.rb
+++ lib/ambix/config/container.rb
@@ -3,6 +3,7 @@
 # rubocop:disable Layout/LineLength
 
 register(:shell, memoize: true) { Ambix::Shell.new }
+register(:inspector, memoize: true) { Ambix::Inspector.new }
 register(:streams_detector, memoize: true) { Ambix::Inspector::StreamsDetector.new }
 
 # rubocop:enable Layout/LineLength
